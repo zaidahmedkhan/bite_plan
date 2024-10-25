a291f035c7d30858d3c6f5194d699766747e2608
John Doe <zaidkhan200415@gmail.com>
2024-10-25 12:28:04 +0500

    animations added

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -5,15 +5,40 @@ import 'package:bite_plan/screens/meals.dart';
 import 'package:bite_plan/widgets/category_grid_item.dart';
 import 'package:flutter/material.dart';
 
-class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen(
-      {super.key,
-      required this.availabaleMeals});
+class CategoriesScreen extends StatefulWidget {
+  const CategoriesScreen({super.key, required this.availabaleMeals});
 
   final List<Meal> availabaleMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animationController.dispose();
+  }
+
   void _selectedCategory(BuildContext context, Category category) {
-    final filteredMeals = availabaleMeals
+    final filteredMeals = widget.availabaleMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
     Navigator.push(
@@ -27,23 +52,34 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      padding: const EdgeInsets.all(24),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectedCategory(context, category);
+              },
+            ),
+        ],
       ),
-      children: [
-        for (final category in availableCategories)
-          CategoryGridItem(
-            category: category,
-            onSelectCategory: () {
-              _selectedCategory(context, category);
-            },
-          ),
-      ],
+      builder: (context, child) => SlideTransition(
+        position: Tween(
+          begin: const Offset(0, 0.3),
+          end: const Offset(0, 0),
+        ).animate(CurvedAnimation(
+            parent: _animationController, curve: Curves.easeInOut)),
+        child: child,
+      ),
     );
   }
 }
--- lib/screens/meal_detail.dart
+++ lib/screens/meal_detail.dart
@@ -32,7 +32,18 @@ class MealDetailScreen extends ConsumerWidget {
                         ? "Meal added as a favorite"
                         : "Meal removed")));
               },
-              icon: Icon(isFavorite ? Icons.star : Icons.star_border))
+              icon: AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 300),
+                  transitionBuilder: (child, animation) {
+                    return RotationTransition(
+                      turns: animation,
+                      child: child,
+                    );
+                  },
+                  child: Icon(
+                    isFavorite ? Icons.star : Icons.star_border,
+                    key: ValueKey(isFavorite),
+                  )))
         ],
       ),
       body: SingleChildScrollView(
